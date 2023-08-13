b95a0db790e2779c3b66d24c4a4586168ff8e637
Ernesto Alejandro <eacarcasses@gmail.com>
2023-08-13 20:27:54 +0200

    Modularización completa

---
+++ chat_view.py
@@ -0,0 +1,69 @@
+class ChatView:
+    def __init__(self, MyTabView, ctk, ia):
+
+        MyTabView.add("CHAT")
+        MyTabView.tab("CHAT").grid_columnconfigure(0, weight=1)
+        MyTabView.tab("CHAT").grid_rowconfigure(0, weight=1)
+
+        frame_principal = ctk.CTkFrame(
+            MyTabView.tab("CHAT"), border_width=-10, fg_color="transparent")
+        frame_principal.grid(row=0, column=0, padx=6, sticky="nsew")
+        frame_principal.grid_rowconfigure(0, weight=1)
+        frame_principal.grid_columnconfigure(0, weight=1)
+
+        cuadro_superior = ctk.CTkFrame(
+            master=frame_principal, fg_color="transparent")
+        cuadro_superior.grid(padx=0, pady=(0, 12), sticky="nswe")
+        cuadro_superior.grid_rowconfigure(0, weight=1)
+        cuadro_superior.grid_columnconfigure(0, weight=1)
+
+        salida = ctk.CTkTextbox(
+            cuadro_superior, fg_color="transparent", scrollbar_button_color="#A8A8A8",
+            scrollbar_button_hover_color="#A8A8A8")
+        salida.grid(row=0, column=0, sticky="nswe")
+        salida.configure(wrap="word")
+        salida.configure(state="disabled")
+
+        cuadro_inferior = ctk.CTkFrame(
+            master=frame_principal, fg_color="transparent")
+        cuadro_inferior.grid(padx=0, pady=(0, 7), sticky="swe")
+        cuadro_inferior.grid_rowconfigure(0, weight=1)
+        cuadro_inferior.grid_columnconfigure(0, weight=3)
+        cuadro_inferior.grid_columnconfigure(1, weight=0)
+
+        entrada = ctk.CTkEntry(cuadro_inferior, placeholder_text_color="#A8A8A8",
+                               placeholder_text="Ingrese una pregunta...")
+        entrada.grid(row=0, column=0, padx=(0, 9), sticky="we")
+        entrada.bind('<Return>', lambda event: enviar())
+
+        contenedor_botones = ctk.CTkFrame(
+            cuadro_inferior, fg_color="transparent")
+        contenedor_botones.grid(row=0, column=1, sticky="ew")
+
+        boton_enviar = ctk.CTkButton(
+            contenedor_botones, text="Enviar", command=lambda: enviar())
+        boton_enviar.grid(row=0, column=0, sticky="ew")
+        boton_enviar.configure(
+            width=60, fg_color="#1E90FF", hover_color="#1A7AD9")
+
+        def enviar():
+            respuesta = ia.preguntar(entrada.get())
+            salida.configure(state="normal")
+            salida.delete(0.0, ctk.END)
+            salida.insert("0.0", respuesta)
+            salida.configure(state="disabled")
+            entrada.delete("0", "end")
+
+        boton_reiniciar = ctk.CTkButton(
+            contenedor_botones, text="\u267A", font=("", 16), command=lambda: reiniciar())
+        boton_reiniciar.grid(row=0, column=1, padx=(10, 0), sticky="ew")
+        boton_reiniciar.configure(
+            width=35, fg_color="#F28A30", hover_color="#CE7529")
+
+        def reiniciar():
+            ia.reiniciar()
+            salida.configure(state='normal')
+            salida.delete(0.0, ctk.END)
+            salida.configure(state='disabled')
+            entrada.delete("0", "end")
+            entrada.insert(0, "Conversación reiniciada...")
---
+++ imagen_view.py
@@ -0,0 +1,58 @@
+class ImagenView:
+    def __init__(self, MyTabView, ctk, ia, urllib, Image, io):
+
+        MyTabView.add("IMAGEN")
+        MyTabView.tab("IMAGEN").grid_columnconfigure(0, weight=1)
+        MyTabView.tab("IMAGEN").grid_rowconfigure(0, weight=1)
+
+        frame_imagen = ctk.CTkFrame(
+            MyTabView.tab("IMAGEN"), border_width=-10, fg_color="transparent")
+        frame_imagen.grid(row=0, column=0, padx=6, sticky="nsew")
+        frame_imagen.grid_rowconfigure(0, weight=1)
+        frame_imagen.grid_columnconfigure(0, weight=1)
+
+        frame_salida_imagen = ctk.CTkFrame(
+            frame_imagen, fg_color="transparent")
+        frame_salida_imagen.grid(row=0, column=0, pady=(0, 12), sticky="nswe")
+        frame_salida_imagen.grid_rowconfigure(0, weight=1)
+        frame_salida_imagen.grid_columnconfigure(0, weight=1)
+
+        frame_entrada_imagen = ctk.CTkFrame(
+            frame_imagen, fg_color="transparent")
+        frame_entrada_imagen.grid(padx=0, pady=(0, 7), sticky="swe")
+        frame_entrada_imagen.grid_rowconfigure(0, weight=1)
+        frame_entrada_imagen.grid_columnconfigure(0, weight=3)
+        frame_entrada_imagen.grid_columnconfigure(1, weight=0)
+
+        entrada_imagen = ctk.CTkEntry(
+            frame_entrada_imagen, placeholder_text_color="#A8A8A8",
+            placeholder_text="Ingrese una descripción...")
+        entrada_imagen.grid(row=0, column=0, padx=(0, 9), sticky="we")
+        entrada_imagen.bind('<Return>', lambda event: crear())
+
+        contenedor_botones_imagen = ctk.CTkFrame(
+            frame_entrada_imagen, fg_color="transparent")
+        contenedor_botones_imagen.grid(row=0, column=1, sticky="ew")
+
+        boton_entrada_imagen = ctk.CTkButton(
+            contenedor_botones_imagen, text="Crear", command=lambda: crear())
+        boton_entrada_imagen.grid(row=0, column=0, sticky="ew")
+        boton_entrada_imagen.configure(
+            width=60, fg_color="#1E90FF", hover_color="#1A7AD9")
+
+        def crear():
+            imagen_url = ia.crear_imagen(entrada_imagen.get())
+            with urllib.request.urlopen(imagen_url) as url_datos:
+                raw_data = url_datos.read()
+            imagen_crear = Image.open(io.BytesIO(raw_data))
+            photo = ctk.CTkImage(imagen_crear, size=(470, 405))
+            label_imagen = ctk.CTkLabel(
+                frame_salida_imagen, image=photo, text="")
+            label_imagen.grid(row=0, column=0, sticky="nswe")
+            entrada_imagen.delete("0", "end")
+
+        boton_descargar_imagen = ctk.CTkButton(
+            contenedor_botones_imagen, text="\u2B07", command=lambda: ia.guardar())
+        boton_descargar_imagen.grid(row=0, column=1, padx=(10, 0), sticky="ew")
+        boton_descargar_imagen.configure(
+            width=35, fg_color="#009E54", hover_color="#008D47")
