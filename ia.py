c39e8228bdc9ee993a4f987f8f0e207db88bbf48
Ernesto Alejandro <eacarcasses@gmail.com>
2024-03-18 17:17:39 +0100

    Added error handling for the "ask" method and some translations from Spanish to English

--- ia.py
+++ ia.py
@@ -3,8 +3,8 @@
 import shelve
 from tkinter.filedialog import asksaveasfilename
 import os
-from openai import OpenAI
 import requests
+from openai import OpenAI, AuthenticationError, OpenAIError
 
 
 class Database:
@@ -19,7 +19,7 @@ class Database:
             os.makedirs("database")
         self.conn = shelve.open("database/iaDB")
 
-    def obtener_data(self):
+    def get_data(self):
         "Gets and returns the values stored in the DB"
         self.open()
         if self.conn is not None:
@@ -35,7 +35,7 @@ class Database:
 
         return None, None, None, None
 
-    def ingresar_data(self, lugar, data):
+    def enter_data(self, lugar, data):
         "Add information to the DB with the Place-Data scheme"
         self.open()
         if self.conn is not None:
@@ -53,47 +53,56 @@ class Openai:
 
     def __init__(self):
         self.database = Database()
-        api_key = self.database.obtener_data()[0]
+        api_key = self.database.get_data()[0]
         if api_key is not None:
             self.client = OpenAI(api_key=str(api_key))
         else:
             self.client = OpenAI(api_key="")
-        self.db_modelo = self.database.obtener_data()[1]
-        self.db_contexto = self.database.obtener_data()[2]
-        self.url = self.database.obtener_data()[3]
+        self.db_modelo = self.database.get_data()[1]
+        self.db_contexto = self.database.get_data()[2]
+        self.url = self.database.get_data()[3]
 
-    def preguntar(self, user_entry):
+    def ask(self, user_entry):
         "From the user's question returns an answer and adds it to a context"
-        self.actualizar()
+        self.update()
         if self.db_contexto is not None:
             self.db_contexto.append({"role": "user", "content": user_entry})
-            respuesta = self.client.chat.completions.create(
-                model=str(self.db_modelo), messages=self.db_contexto)
-            respuesta = respuesta.choices[0].message.content
-            self.db_contexto.append(
-                {"role": "assistant", "content": respuesta})
-            return respuesta
+            try:
+                respuesta = self.client.chat.completions.create(
+                    model=str(self.db_modelo), messages=self.db_contexto)
+                respuesta = respuesta.choices[0].message.content
+                self.db_contexto.append(
+                    {"role": "assistant", "content": respuesta})
+                return respuesta
+
+            except AuthenticationError:
+                # Capture the authentication error and display an appropriate message to the user
+                return "Invalid API Key"
+
+            except OpenAIError:
+                # Capture other OpenAI errors and display an appropriate message to the user
+                return "Model not found"
         return None
 
-    def actualizar(self):
+    def update(self):
         "Synchronize and compare the values of the DB with those of the application instance"
-        if self.client.api_key != self.database.obtener_data()[0]:
-            self.client.api_key = str(self.database.obtener_data()[0])
-        if self.db_modelo != self.database.obtener_data()[1]:
-            self.db_modelo = self.database.obtener_data()[1]
+        if self.client.api_key != self.database.get_data()[0]:
+            self.client.api_key = str(self.database.get_data()[0])
+        if self.db_modelo != self.database.get_data()[1]:
+            self.db_modelo = self.database.get_data()[1]
 
-    def crear_imagen(self, user_entry):
+    def create_image(self, user_entry):
         "Create an image from a user description"
         response = self.client.images.generate(
             prompt=user_entry, n=1, size="1024x1024")
         imagen_url = response.data[0].url
         print(imagen_url)
-        self.database.ingresar_data("url", imagen_url)
+        self.database.enter_data("url", imagen_url)
         return imagen_url
 
-    def guardar(self):
+    def save(self):
         "Save the generated image in the system"
-        url = self.database.obtener_data()[3]
+        url = self.database.get_data()[3]
         if url is None:
             raise ValueError("URL is None")
         response = requests.get(url, timeout=10)
@@ -101,6 +110,6 @@ class Openai:
         with open(ruta_guardado, "wb") as archivo:
             archivo.write(response.content)
 
-    def reiniciar(self):
+    def restart(self):
         "Restarts the DB context"
-        self.db_contexto = self.database.obtener_data()[2]
+        self.db_contexto = self.database.get_data()[2]
